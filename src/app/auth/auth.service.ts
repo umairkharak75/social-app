d360184030007d9921998070a369080e1d27d99f
Entertainment Overloaded <umairkharak75@gmail.com>
2020-06-24 10:26:45 -0700

    lint gix

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,11 +8,11 @@ export class AuthService {
 
   constructor(public http:HttpClient) { }
   createUser(userData){
-   return this.http.post('http://localhost:5000/api/users',userData)
+   return this.http.post('api/users',userData)
   }
 
   login(data){
-    return this.http.post('http://localhost:5000/api/auth',data)
+    return this.http.post('api/auth',data)
 
   }
 }
