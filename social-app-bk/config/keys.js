72f08ff9c78cb0a7e93010420ab43d75dcd78fe2
Entertainment Overloaded <umairkharak75@gmail.com>
2020-06-24 09:15:01 -0700

    initail commit

---
+++ social-app-bk/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongodb: "mongodb+srv://umairkharak75:1234567890@sa-backend-cnw8q.mongodb.net/sa-backend?retryWrites=true&w=majority",
+	jwtSecret: "Silencio is a villainsio",
+
+ 
+};
