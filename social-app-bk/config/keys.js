63c355bcd5a2561b69ad43827cbef09dfc786d23
Entertainment Overloaded <umairkharak75@gmail.com>
2020-06-24 13:38:14 -0700

    issue fix

--- social-app-bk/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongodb: "mongodb+srv://umairkharak75:1234567890@sa-backend-cnw8q.mongodb.net/sa-backend?retryWrites=true&w=majority",
-	jwtSecret: "Silencio is a villainsio",
-
- 
-};
